530ab0455ef45c7246dbfd7d682c0bfe18b565d2
peterzx000 <39615640+peterzx000@users.noreply.github.com>
2022-05-27 10:32:51 +0800

    Update cube_simulation.py

--- cube_simulation.py
+++ cube_simulation.py
@@ -1,42 +1,51 @@
 import cv2
 import numpy as np
-import math
+import configparser
 
 class cube:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,file):
+        with np.load(file + '.npz') as file:
+            self.mtx,self.dist=[file[i] for i in ['mtx','dist']] 
+        # config = configparser.ConfigParser()
+        # config.read('calibration.ini')
+        # self.mtx = []
+        # self.dist = []
+        # for i in config['mtx']:
+        #     self.mtx.append(int(i))
+        # for i in config['dis']:
+        #     self.dist.append(int(i))
 
-    def find_corners(self,image):
-        size = 30
-        objp = np.zeros((6*9,3), np.float32)
-        objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)*size
-        objpoints = []
-        imagepoints = []
-        gray=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
-        ret,corners=cv2.findChessboardCorners(gray,(9,6),None) 
-        if ret == True:
-            objpoints.append(objp)
-            imagepoints.append(corners)
-            return objpoints,imagepoints
-        else:
-            return False
+    # def find_corners(self,image):
+    #     size = 30
+    #     objp = np.zeros((6*9,3), np.float32)
+    #     objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)*size
+    #     objpoints = []
+    #     imagepoints = []
+    #     gray=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+    #     ret,corners=cv2.findChessboardCorners(gray,(9,6),None) 
+    #     if ret == True:
+    #         objpoints.append(objp)
+    #         imagepoints.append(corners)
+    #         return objpoints,imagepoints
+    #     else:
+    #         return False
 
     #first time run, create calibration config with chess board
-    def calibrate(self,img):
-        img = cv2.resize(img, (1920, 1080), interpolation=cv2.INTER_AREA)
-        cv2.imshow('calibrate',img)
-        cv2.waitKey(1)
-        conrner = self.find_corners(img)
-        if conrner!=False:
-            objpoints, imagepoints = conrner
-        else:
-            return False
-        cv2.destroyAllWindows()
+    # def calibrate(self,img):
+    #     img = cv2.resize(img, (1920, 1080), interpolation=cv2.INTER_AREA)
+    #     cv2.imshow('calibrate',img)
+    #     cv2.waitKey(1)
+    #     conrner = self.find_corners(img)
+    #     if conrner!=False:
+    #         objpoints, imagepoints = conrner
+    #     else:
+    #         return False
+    #     cv2.destroyAllWindows()
 
-        im_shape=(1920, 1080)
-        __, mtx, dist, __, __ = cv2.calibrateCamera(objpoints, imagepoints,im_shape,None,None)
+    #     im_shape=(1920, 1080)
+    #     __, mtx, dist, __, __ = cv2.calibrateCamera(objpoints, imagepoints,im_shape,None,None)
 
-        np.savez('camera_matrix',mtx=mtx,dist=dist)
+    #     np.savez('camera_matrix',mtx=mtx,dist=dist)
 
     def draw_cube(self,img, corners, imgpts):
         imgpts = np.int32(imgpts).reshape(-1,2)
@@ -108,10 +117,6 @@ class cube:
                 t = np.append((corner[0]+x_v1*i+(corner[1] + x_v2*i -corner[0]-x_v1*i)/5*j),[0])
                 l.append(t)
         self.objp = np.array(l,np.float32)
-
-    def read_config(self):
-        with np.load('camera_matrix.npz') as file:
-            self.mtx,self.dist=[file[i] for i in ['mtx','dist']] 
 
     # def show(self,img,polygon):
     def show(self,img,corner):
